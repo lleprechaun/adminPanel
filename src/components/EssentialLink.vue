22f056637bfe6f932a694cb6ae979992c6711dd9
Валерия Елганова <valeriaelganova@MacBook-Pro-Valeria.local>
2022-04-12 22:59:29 +0400

    Initial commit

---
+++ src/components/EssentialLink.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-list bordered separator>
+    <q-item
+      clickable v-ripple
+      tag="a"
+      target="_blank"
+      @click="selectedLink"
+    >
+      <q-item-section>
+        <q-item-label>{{ title }}</q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'EssentialLink',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    link: {
+      type: String,
+      default: '#'
+    }
+  },
+  methods:
+    {
+      selectedLink () {
+        this.$emit('selectedLink', this.title)
+      }
+    }
+})
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,108 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="q-ma-none">
+      <q-toolbar>
+
+        <q-toolbar-title class="col-3 text-center q-ml-xl" style="border-right: solid white 2px ">
+          Админ-панель
+        </q-toolbar-title>
+
+        <div
+          class="col-9 text-center text-h6"
+        >{{selectLink}}</div>
+      </q-toolbar>
+    </q-header>
+  <div class="row">
+    <div class="col-3 q-ml-xl q-mt-xl q-mr-none">
+      <q-page-container
+        v-model="leftDrawerOpen"
+        show-if-above
+        bordered
+      >
+        <q-list>
+          <div class="text-center">
+          <EssentialLink
+            v-for="link in essentialLinks"
+            :key="link.title"
+            v-bind="link"
+            @selectedLink="selectedLink"
+          />
+          </div>
+        </q-list>
+      </q-page-container>
+    </div>
+    <div class="col-9">
+      <q-page-container>
+      </q-page-container>
+    </div>
+  </div>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Пользователи',
+    link: ''
+  },
+  {
+    title: 'Редактирование ставок',
+    link: ''
+  },
+  {
+    title: 'Редактирование раздела "Вопросы - ответы"',
+    link: ''
+  },
+  {
+    title: 'Редактирование реферальной программы',
+    link: ''
+  },
+  {
+    title: 'Баги и ошибки',
+    link: ''
+  },
+  {
+    title: 'Создание объявлений',
+    link: ''
+  },
+  {
+    title: 'Табеля',
+    link: ''
+  },
+  {
+    title: 'Документы',
+    link: ''
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+  data () {
+    return {
+      selectLink: 'Пользователи'
+    }
+  },
+  components: {
+    EssentialLink
+  },
+  methods: {
+    selectedLink (title) {
+      this.selectLink = title
+    }
+  },
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
